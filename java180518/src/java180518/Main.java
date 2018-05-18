e65421c7a19267d937449f5612b3aab9b6b27c05
smhrc16 <smhrc16@DESKTOP-GS8T8JA>
2018-05-18 11:50:09 +0900

    1. Main 클래스 작성 중! 수정과 삭제 추가해야합니다. 2. Member클래스 완성 !

--- java180518/src/java180518/Main.java
+++ java180518/src/java180518/Main.java
@@ -1,13 +1,57 @@
 package java180518;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
-		System.out.println("안녕하세요");
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1.가입  2.조회  3.수정 4.삭제");
+		int choice = sc.nextInt();
+
+		if (choice == 1) {
+			System.out.print("이름 : ");
+			String name = sc.next();
+			System.out.print("나이 : ");
+			int age = sc.nextInt();
+			System.out.print("전화번호 : ");
+			String tel = sc.next();
+			System.out.print("고객번호 : ");
+			String memNum = sc.next();
+
+			Member mem = new Member(name, age, tel, memNum);
+			// MemberDAO dao = new MemberDAO();
+			// dao.insert(mem);
+
+		} else if (choice == 2) {
+
+			System.out.print("고객번호 : ");
+			String memNum = sc.next();
+			// MemberDAO dao = new MemberDAO();
+			// Member m = dao.selectOne(memNum);
+			// System.out.println("이름/나이/전화번호/고객번호");
+			// System.out.println(m.getName()+"/"+m.getAge()+"/"+m.getTel()+"/"+m.getMemNum());
+
+		} else if (choice == 3) {
+			System.out.println("고객번호 : ");
+			String memNum = sc.next();
+			
+			//MemberUpdate update = new MemberUpdate();
+			//클래스에서 가져올 m! 구문 작성 
+			System.out.println("수정할 내용을 적어주세요.");
+			System.out.println("이름/나이/전화번호/고객번호");
+			System.out.println();
+			
+
+		} else if (choice == 4) {
+			System.out.println("고객번호 : ");
+			String memNum = sc.next();
+			
+			
+
+		}
+
 	}
 
 }
