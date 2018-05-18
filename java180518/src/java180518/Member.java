4502d7c2e40056ff98648f6a64d7a1c29567a66a
smhrc21 <smhrc21@DESKTOP-QJNEO8A>
2018-05-18 11:46:20 +0900

    MemberDAO 	수정, 삭제 기능추가

--- java180518/src/java180518/Member.java
+++ java180518/src/java180518/Member.java
@@ -1,5 +1,50 @@
 package java180518;
 
 public class Member {
+	private String name;
+	private int age;
+	private String tel;
+	private String memnum;
+	
+	public Member(String name, int age, String tel, String memnum) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.tel = tel;
+		this.memnum = memnum;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getMemnum() {
+		return memnum;
+	}
+
+	public void setMemnum(String memnum) {
+		this.memnum = memnum;
+	}
+	
+	
 }
