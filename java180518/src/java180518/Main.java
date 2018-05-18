f27b49f9fc6a33e0f477751d09bf9907cc4f29b9
smhrc16 <smhrc16@DESKTOP-GS8T8JA>
2018-05-18 12:08:02 +0900

    1. while문안에 무한루프를 돌렸습니다. 2. 다른건 다 되는데 .. 삭제 후 삭제된 고객정보를 조회했을때 오류가 발생합니다. 3. 그 부분만 완성되면 될 것 같습니다.

--- java180518/src/java180518/Main.java
+++ java180518/src/java180518/Main.java
@@ -7,57 +7,64 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("1.가입  2.조회  3.수정 4.삭제");
-		int choice = sc.nextInt();
+		boolean a = true;
 
-		if (choice == 1) {
-			System.out.print("이름 : ");
-			String name = sc.next();
-			System.out.print("나이 : ");
-			int age = sc.nextInt();
-			System.out.print("전화번호 : ");
-			String tel = sc.next();
-			System.out.print("고객번호 : ");
-			String memNum = sc.next();
+		while (a) {
+			System.out.println("1.가입  2.조회  3.수정 4.삭제 5.종료");
+			int choice = sc.nextInt();
 
-			Member mem = new Member(name, age, tel, memNum);
-			MemberDAO dao = new MemberDAO();
-			dao.insert(mem);
+			if (choice == 1) {
+				System.out.print("이름 : ");
+				String name = sc.next();
+				System.out.print("나이 : ");
+				int age = sc.nextInt();
+				System.out.print("전화번호 : ");
+				String tel = sc.next();
+				System.out.print("고객번호 : ");
+				String memNum = sc.next();
 
-		} else if (choice == 2) {
+				Member mem = new Member(name, age, tel, memNum);
+				MemberDAO dao = new MemberDAO();
+				dao.insert(mem);
 
-			System.out.print("고객번호 : ");
-			String memNum = sc.next();
-			MemberDAO dao = new MemberDAO();
-			Member m = dao.selectOne(memNum);
-			System.out.println("이름/나이/전화번호/고객번호");
-			System.out.println(m.getName() + "/" + m.getAge() + "/" + m.getTel() + "/" + m.getMemnum());
+			} else if (choice == 2) {
 
-		} else if (choice == 3) {
-			System.out.println("고객번호 : ");
-			String memNum = sc.next();
-			MemberDAO dao = new MemberDAO();
-			System.out.println("수정할 내용을 적어주세요.");
-			System.out.print("이름 : ");
-			String name = sc.next();
-			System.out.print("나이 : ");
-			int age = sc.nextInt();
-			System.out.print("전화번호 : ");
-			String tel = sc.next();
-			System.out.print("고객번호 : ");
-			String memnum = sc.next();
+				System.out.print("고객번호 : ");
+				String memNum = sc.next();
+				MemberDAO dao = new MemberDAO();
+				Member m = dao.selectOne(memNum);
+				System.out.println("이름/나이/전화번호/고객번호");
+				System.out.println(m.getName() + "/" + m.getAge() + "/" + m.getTel() + "/" + m.getMemnum());
+				
+				
 
-			Member m = dao.MemberUpdate(name, age, tel, memnum);
+			} else if (choice == 3) {
+				System.out.println("고객번호 : ");
+				String memNum = sc.next();
+				MemberDAO dao = new MemberDAO();
+				System.out.println("수정할 내용을 적어주세요.");
+				System.out.print("이름 : ");
+				String name = sc.next();
+				System.out.print("나이 : ");
+				int age = sc.nextInt();
+				System.out.print("전화번호 : ");
+				String tel = sc.next();
+				System.out.print("고객번호 : ");
+				String memnum = sc.next();
 
-		} else if (choice == 4) {
+				Member m = dao.MemberUpdate(name, age, tel, memnum);
 
-			System.out.println("고객번호 : ");
-			String memNum = sc.next();
-			MemberDAO dao = new MemberDAO();
-			Member m = dao.MemberDelete(memNum);
+			} else if (choice == 4) {
 
-			
+				System.out.println("고객번호 : ");
+				String memNum = sc.next();
+				MemberDAO dao = new MemberDAO();
+				Member m = dao.MemberDelete(memNum);
 
+			}else if(choice == 5) {
+				System.out.println("시스템을 종료하겠습니다.");
+				a=false;
+			}
 		}
 
 	}
