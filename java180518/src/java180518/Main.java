7dd9703d0f2c615ccc55b2a196e54e2a2bbfe10a
smhrc16 <smhrc16@DESKTOP-GS8T8JA>
2018-05-18 12:01:25 +0900

    4.delete까지 완료

--- java180518/src/java180518/Main.java
+++ java180518/src/java180518/Main.java
@@ -21,37 +21,44 @@ public class Main {
 			String memNum = sc.next();
 
 			Member mem = new Member(name, age, tel, memNum);
-			// MemberDAO dao = new MemberDAO();
-			// dao.insert(mem);
+			MemberDAO dao = new MemberDAO();
+			dao.insert(mem);
 
 		} else if (choice == 2) {
 
 			System.out.print("고객번호 : ");
 			String memNum = sc.next();
-			// MemberDAO dao = new MemberDAO();
-			// Member m = dao.selectOne(memNum);
-			// System.out.println("이름/나이/전화번호/고객번호");
-			// System.out.println(m.getName()+"/"+m.getAge()+"/"+m.getTel()+"/"+m.getMemNum());
+			MemberDAO dao = new MemberDAO();
+			Member m = dao.selectOne(memNum);
+			System.out.println("이름/나이/전화번호/고객번호");
+			System.out.println(m.getName() + "/" + m.getAge() + "/" + m.getTel() + "/" + m.getMemnum());
 
 		} else if (choice == 3) {
 			System.out.println("고객번호 : ");
 			String memNum = sc.next();
-			
-			//MemberUpdate update = new MemberUpdate();
-			//클래스에서 가져올 m! 구문 작성 
+			MemberDAO dao = new MemberDAO();
 			System.out.println("수정할 내용을 적어주세요.");
-			System.out.println("이름/나이/전화번호/고객번호");
-			System.out.println();
-			
+			System.out.print("이름 : ");
+			String name = sc.next();
+			System.out.print("나이 : ");
+			int age = sc.nextInt();
+			System.out.print("전화번호 : ");
+			String tel = sc.next();
+			System.out.print("고객번호 : ");
+			String memnum = sc.next();
+
+			Member m = dao.MemberUpdate(name, age, tel, memnum);
 
 		} else if (choice == 4) {
+
 			System.out.println("고객번호 : ");
 			String memNum = sc.next();
-			
+			MemberDAO dao = new MemberDAO();
+			Member m = dao.MemberDelete(memNum);
+
 			
 
 		}
 
 	}
-
 }
